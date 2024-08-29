01c086550ed3de2859378a1d9b5fd2fd9b1641e2
ElectraBytes04 <electra-dev.404@protonmail.com>
2024-08-29 10:41:08 -0400

    Made title screen

---
+++ tanks-pysc.py
@@ -0,0 +1,30 @@
+def main(): 
+    print("""
+,---------------------------------------------------------------,
+|\| | | | | | | |  ####   ##   ## #  #  #  /### | | | | | | | |/|
+|/| | | | | | | |  #    #--#  # ##  ###   \--\  | | | | | | | |\|
+|\| | | | | | | | #    #  #  #  #  #  #  ###/   | | | | | | | |/|
+|===============================================================|
+|W: Up/Forward                                BY ElectraBytes04 |
+|A: (Look) Left/Left											|
+|S: Down/Back													|
+|D: (Look) Right/Right											|
+|E: Interact/[Enter]											|
+|F: Menu/[...]													|
+|Q: Shoot/[...]													|
+|		#  * * *										☺		|
+|																|
+|[][][][][]														|
+|					<==- TYPE [E] TO START -==>					|
+|	•															|
+|	•														[]	|
+|	•														[]	|
+|															[]	|
+|	▲														[]  |
+'---------------------------------------------------------------'
+""")
+# @todo: 
+# ask phind how to position cursor at "0, 0"; 
+# find other way to clear screen
+# use dicts to keep and update different screens;
+# get input without pressing enter??;
